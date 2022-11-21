cef15e39e7305fa914fb5395feb818d874cc6829
Win Mon <winmon.info@gmail.com>
2022-11-21 15:35:38 +0630

    first setup for food_store application service commit

---
+++ src/test/java/com/ojt/bs/FoodStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojt.bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
